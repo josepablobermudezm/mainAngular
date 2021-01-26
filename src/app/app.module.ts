095042ab73cbbef1c00a9af26d7ef70440c94e5f
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2021-01-26 00:32:22 -0600

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { FormsModule } from '@angular/forms';
 import { SquareComponent } from './tictactoe/square/square.component';
 import { BoardComponent } from './tictactoe/board/board.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [AppComponent, DashboardComponent, ContactsComponent, TodoListComponent, SquareComponent, BoardComponent],
-  imports: [BrowserModule, AppRoutingModule, SidebarModule.forRoot(), FormsModule, FontAwesomeModule ],
+  imports: [BrowserModule, AppRoutingModule, SidebarModule.forRoot(), FormsModule, FontAwesomeModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }) ],
   providers: [],
   bootstrap: [AppComponent],
 })
