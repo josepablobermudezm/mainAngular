c97b618163ea9567f315f959e4013ee23b9935dc
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2021-01-26 20:08:13 -0600

    act #57

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,11 +13,44 @@ import { SquareComponent } from './tictactoe/square/square.component';
 import { BoardComponent } from './tictactoe/board/board.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment'
+import { environment } from '../environments/environment';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, ContactsComponent, TodoListComponent, SquareComponent, BoardComponent],
-  imports: [BrowserModule, AppRoutingModule, SidebarModule.forRoot(), FormsModule, FontAwesomeModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }) ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ContactsComponent,
+    TodoListComponent,
+    SquareComponent,
+    BoardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SidebarModule.forRoot(),
+    FormsModule,
+    FontAwesomeModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    CommonModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { AngularFirestore } from '@angular/fire/firestore';
+import { tap, first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.scss'],
 })
 export class ContactsComponent implements OnInit {
+  myForm: FormGroup;
 
-  constructor() { }
+  //form state
+  loading = false;
+  success = false;
+
+  constructor(private fb: FormBuilder/*, private afs: AngularFirestore*/) {}
 
   ngOnInit(): void {
+    this.myForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required]],
+    });
+    //this.myForm.valueChanges.subscribe(console.log);
   }
 
+  get email() {
+    return this.myForm.get('email');
+  }
+
+  get message() {
+    return this.myForm.get('message');
+  }
+
+  /*async submitHandler() {
+    this.loading = true;
+    const formValue = this.myForm.value;
+    try {
+      await this.afs.collection('contacts').add(formValue);
+      this.success = true;
+    } catch (err) {
+      console.log(err);
+    }
+
+    this.loading = false;
+  }*/
 }
